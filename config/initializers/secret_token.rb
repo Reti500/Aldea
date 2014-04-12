f393d9604d323e4eab8dcc8a59e8be6ad2aad92f
Ricardo <chaneque500@gmail.com>
2014-04-12 00:40:44 -0500

    Primera version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Aldea::Application.config.secret_key_base = 'efccf88c2e27898a6c86570e72205279716247fc8a01b2e0a372357a2873e3ae24aaee47e800603bd9da87661504162dd9dd6fa7cc4a32e64343450fea3c467b'
