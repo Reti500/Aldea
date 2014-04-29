c2d04663d1942968684bbba5e0c21fb526cddf62
Ricardo <chaneque500@gmail.com>
2014-04-28 23:47:21 -0500

    Fecha y hora

--- app/serializers/codigo_serializer.rb
+++ app/serializers/codigo_serializer.rb
@@ -8,4 +8,12 @@ class CodigoSerializer < ActiveModel::Serializer
 
   	return nil
   end
+
+  def fecha
+  	object.created_at.to_date
+  end
+
+  def hora
+  	object.created_at.strftime("%I:%M%p")
+  end
 end
